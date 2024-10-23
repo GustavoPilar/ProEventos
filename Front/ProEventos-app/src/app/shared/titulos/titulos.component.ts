05d89487fdd2dc36f54937c8d58d35387ee6e5c7
Gustavo Pilar <gustavo.silva@weipa.com.br>
2024-10-23 15:58:22 -0300

    Seção 8

--- Front/ProEventos-app/src/app/shared/titulos/titulos.component.ts
+++ Front/ProEventos-app/src/app/shared/titulos/titulos.component.ts
@@ -1,21 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-titulos',
   standalone: true,
   imports: [
-    
+    CommonModule
   ],
   templateUrl: './titulos.component.html',
   styleUrl: './titulos.component.scss'
 })
 export class TitulosComponent implements OnInit {
   
-  @Input ({
+  @Input({
     alias: 'titulo',
     required: true,
-    transform: (value: string) => value.toUpperCase()
-  }) titulo!: string;
+  }) titulo = '';
+
+  @Input({
+    alias: 'subtitulo',
+    required: false,
+  }) subtitulo = 'Desde 2021';
+
+  @Input({
+    alias: 'iconClass',
+    required: false
+  }) iconClass = 'fa fa-user';
+
+  @Input({}) botaoListar = false;
 
   constructor() { }
 
